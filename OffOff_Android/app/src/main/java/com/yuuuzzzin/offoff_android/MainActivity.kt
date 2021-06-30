667151b6f27d6b10d88b0ca45f77322a8b1bd2fd
yuuuzzzin <uiuiui118437@gmail.com>
2021-07-01 00:21:08 +0900

    Android Project 생성

---
+++ OffOff_Android/app/src/main/java/com/yuuuzzzin/offoff_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yuuuzzzin.offoff_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
