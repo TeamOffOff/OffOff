c371e867556b5d6a20b20541515a3dd57c0d8fd8
YouminYoumin <dbals2007@naver.com>
2021-07-03 19:32:00 +0900

    회원 가입 및 로그인 로직 작성

--- python_server/main.py
+++ python_server/main.py
@@ -1,19 +1,39 @@
 from flask import Flask, request, jsonify  # 서버 구현을 위한 Flask 객체 import
 from flask_restx import Api, Resource
 
+import mongo
+
 app = Flask(__name__)
 api = Api(app)
 
 
 @api.route("/SignUp")
 class SignUp(Resource):
-    pass
+    def post(self):
+        user_info = request.get_json()
+
+        if not mongodb.find_one({"id": user_info["id"]}):
+            mongodb.insert_one(user_info)
+            return {"query_status": "success"}
+        else:
+            return {"query_status": "fail"}
 
 
 @api.route("/SignIn")
 class SignIn(Resource):
-    pass
+    def post(self):
+        sign_info = request.get_json()
+        user_info = mongodb.find_one({"id": sign_info["id"]})
+
+        if not user_info:
+            return {"query_status": "유효하지않은 id입니다."}
+
+        if user_info["password"] != sign_info["password"]:
+            return {"query_status": "틀린 비밀번호입니다."}
+        else:
+            return {"query_status": "success"}
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    mongodb = mongo.MongoHelper()
+    app.run(debug=True, host="0.0.0.0", port=5000)
--- python_server/mongo.py
+++ python_server/mongo.py
@@ -11,22 +11,37 @@ class MongoHelper:
         self.db = self.client[db_name]
         self.collection = self.db[collection_name]
 
-    def insert(self, data=None):
+    def insert_one(self, data=None):
         result = self.collection.insert_one(data).inserted_id
         return result
 
-    def search(self, query=None):
+    def insert_many(self, data=None):
+        result = self.collection.insert_many(data).inserted_ids
+        return result
+
+    def find_one(self, query=None):
         result = self.collection.find_one(query)
         return result
 
-    def delete(self, query=None):
+    def find(self, query=None):
+        result = self.collection.find(query)
+        return result
+
+    def delete_one(self, query=None):
         result = self.collection.delete_one(query)
+        return result
+
+    def delete_many(self, query=None):
+        result = self.collection.delete_many(query)
         return result
 
 
 if __name__ == "__main__":
     mongodb = MongoHelper()
 
-    print(mongodb.insert({"test": "hi"}))
-    print(mongodb.search())
-    print(mongodb.delete())
+    print(mongodb.insert_one({"test": "hi"}))
+    for string in mongodb.find():
+        print(string)
+    print(mongodb.delete_many({"test": "hi"}))
+    for string in mongodb.find():
+        print(string)
