9afcd83ad45e50db3127d1d3ae059c19052d6cf1
yuuuzzzin <uiuiui118437@gmail.com>
2021-07-06 01:25:07 +0900

    툴바 메뉴 선택에 따른 액티비티 이동 구현

--- OffOff_Android/app/src/main/java/com/yuuuzzzin/offoff_android/view/ui/HomeFragment.kt
+++ OffOff_Android/app/src/main/java/com/yuuuzzzin/offoff_android/view/ui/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.yuuuzzzin.offoff_android.view.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toolbar
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.appbar.MaterialToolbar
 import com.yuuuzzzin.offoff_android.R
 import com.yuuuzzzin.offoff_android.databinding.FragmentHomeBinding
 
@@ -19,8 +23,23 @@ class HomeFragment : Fragment() {
     ): View? {
 
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
-
         mBinding = binding
+
+        val toolbar : MaterialToolbar = binding.appbarHome // 상단 툴바
+
+        toolbar.setOnMenuItemClickListener{
+            when(it.itemId) {
+                R.id.search -> {
+                    startActivity(Intent(context, SearchActivity::class.java))
+                    true
+                }
+                R.id.user -> {
+                    startActivity(Intent(context, UserActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
 
         return mBinding?.root
     }
