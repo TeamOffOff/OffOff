4450ecf6254ff9bb672a28067a20526f13f809e6
YouminYoumin <dbals2007@naver.com>
2021-07-07 21:32:24 +0900

    기본 게시글 추가, 조회, 삭제 기능 구현

--- python_server/main.py
+++ python_server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify  # 서버 구현을 위한 Flask 객체 import
+from flask import Flask, request  # 서버 구현을 위한 Flask 객체 import
 from flask_restx import Api, Resource
 
 import mongo
@@ -12,7 +12,7 @@ class SignUp(Resource):
     def post(self):
         user_info = request.get_json()
 
-        if not mongodb.find_one({"id": user_info["id"]}):
+        if not mongodb.find_one(query={"id": user_info["id"]}, collection_name="user_collection"):
             mongodb.insert_one(user_info)
             return {"query_status": "success"}
         else:
@@ -23,15 +23,47 @@ class SignUp(Resource):
 class SignIn(Resource):
     def post(self):
         sign_info = request.get_json()
-        user_info = mongodb.find_one({"id": sign_info["id"]})
+        user_info = mongodb.find_one(query={"id": sign_info["id"]}, collection_name="user_collection")
 
         if not user_info:
-            return {"query_status": "유효하지않은 id입니다."}
+            return {"query_status": "유효하지않은 ID 입니다."}
 
         if user_info["password"] != sign_info["password"]:
             return {"query_status": "틀린 비밀번호입니다."}
         else:
             return {"query_status": "success"}
+
+
+@api.route("/PutPost")
+class PutPost(Resource):
+    def post(self):
+        post_info = request.get_json()
+
+        if not mongodb.find_one(query={"content_id": post_info["content_id"]}, collection_name="post_collection"):
+            mongodb.insert_one(data=post_info, collection_name="post_collection")
+            return {"query_status": "success"}
+        else:
+            return {"query_status": "중복된 id입니다."}
+
+
+@api.route("/GetPost/content_id=<string:post_id>")
+class GetPost(Resource):
+    def get(self, post_id):
+        post_info = mongodb.find_one(query={"content_id": post_id}, collection_name="post_collection")
+        del post_info["_id"]
+
+        if not post_info:
+            return {"query_status": "해당 id의 게시글을 찾을 수 없습니다."}
+        else:
+            return post_info
+
+
+@api.route("/DeletePost/content_id=<string:post_id>")
+class DeletePost(Resource):
+    def delete(self, post_id):
+        mongodb.delete_one(query={"content_id": post_id}, collection_name="post_collection")
+
+        return {"query_status": "success"}
 
 
 if __name__ == "__main__":
--- python_server/mongo.py
+++ python_server/mongo.py
@@ -3,35 +3,41 @@ from pymongo.cursor import CursorType
 
 
 class MongoHelper:
-    def __init__(self, db_name="test_db", collection_name="test_collection"):
+    def __init__(self, db_name="test_db"):
         print("Run MongoDB")
         self.host = "localhost"
         self.port = 27017
         self.client = MongoClient(self.host, self.port)
         self.db = self.client[db_name]
-        self.collection = self.db[collection_name]
+        self.collection = None
 
-    def insert_one(self, data=None):
+    def insert_one(self, data=None, collection_name=None):
+        self.collection = self.db[collection_name]
         result = self.collection.insert_one(data).inserted_id
         return result
 
-    def insert_many(self, data=None):
+    def insert_many(self, data=None, collection_name=None):
+        self.collection = self.db[collection_name]
         result = self.collection.insert_many(data).inserted_ids
         return result
 
-    def find_one(self, query=None):
+    def find_one(self, query=None, collection_name=None):
+        self.collection = self.db[collection_name]
         result = self.collection.find_one(query)
         return result
 
-    def find(self, query=None):
+    def find(self, query=None, collection_name=None):
+        self.collection = self.db[collection_name]
         result = self.collection.find(query)
         return result
 
-    def delete_one(self, query=None):
+    def delete_one(self, query=None, collection_name=None):
+        self.collection = self.db[collection_name]
         result = self.collection.delete_one(query)
         return result
 
-    def delete_many(self, query=None):
+    def delete_many(self, query=None, collection_name=None):
+        self.collection = self.db[collection_name]
         result = self.collection.delete_many(query)
         return result
 
@@ -42,6 +48,6 @@ if __name__ == "__main__":
     print(mongodb.insert_one({"test": "hi"}))
     for string in mongodb.find():
         print(string)
-    print(mongodb.delete_many({"test": "hi"}))
+    print(mongodb.delete_many({}))
     for string in mongodb.find():
         print(string)
