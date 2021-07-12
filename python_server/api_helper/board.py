c41bdff0400d3520efce631f5b03d3ae3aef0886
ssooda <84752537+ssooda@users.noreply.github.com>
2021-07-12 21:36:57 +0900

    return 수정

--- python_server/api_helper/board.py
+++ python_server/api_helper/board.py
@@ -17,9 +17,9 @@ class ListControl(Resource) :
         
         #사용자가 위에서 아래로 스크롤하면 이전에 봤던 페이지로 인식해서 그걸 넘겨주고 (page는 -1)
         #사용자가 아래에서 위로 스크롤하면 새로운 페이지를 불러온다고 인식해서 그걸 넘겨줌 (page는 +1)
-        mongodb.find(collection_name = board_type).skip((page-1)*10).limit(10) #스킵할 건 하고(skip) 갯수제한해서(limit) 불러옴 : 10개씩 보여주는 경우
+        post_list = mongodb.find(collection_name = board_type).skip((page-1)*10).limit(10) #스킵할 건 하고(skip) 갯수제한해서(limit) 불러옴 : 10개씩 보여주는 경우
         #db.collection_name.find({}) 해당 컬렉션에 있는 모든 Document를 불러옴
     
-        return page #일단 이 함수가 실행되면 page값을 리턴해주는 걸로
+        return post_list, page #일단 이 함수가 실행되면 page값을 리턴해주는 걸로
         
 
