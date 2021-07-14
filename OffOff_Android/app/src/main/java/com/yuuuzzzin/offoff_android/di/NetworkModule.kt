0a781f64a5ab0c7c47f8f688dc791f1cbf24b641
yuuuzzzin <uiuiui118437@gmail.com>
2021-07-15 03:20:53 +0900

    의존성 주입을 위한 모듈 생성

---
+++ OffOff_Android/app/src/main/java/com/yuuuzzzin/offoff_android/di/NetworkModule.kt
@@ -0,0 +1,36 @@
+package com.yuuuzzzin.offoff_android.di
+
+import com.yuuuzzzin.offoff_android.service.api.BoardService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+/* 네트워크 통신을 위한 모듈 */
+
+const val BASE_URL = "http:10.0.2.2:3000/"
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    /* Retrofit2 통신 모듈 */
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    /* Post API를 위한 모듈 */
+    @Singleton
+    @Provides
+    fun provideBoardService(retrofit: Retrofit): BoardService =
+        retrofit.create(BoardService::class.java)
+
+}
