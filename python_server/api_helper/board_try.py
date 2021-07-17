9ee9f0c20167f377a2c51741b74a62d8cffbae99
ssooda <84752537+ssooda@users.noreply.github.com>
2021-07-18 02:42:43 +0900

    postman test용
    데이터베이스에 데이터 넣은 다음 GET 가능한지

--- python_server/api_helper/board_try.py
+++ python_server/api_helper/board_try.py
@@ -2,6 +2,7 @@
 #게시판 목록 중 -> 자유게시판 클릭 -> board_type 넘겨주면서 /postlist로 연결되게 -> 우선 20개 먼저 보내줌 (컬랙션 : 개별 board_type이름)
 #자유게시판에서 스크롤 -> board_type, 마지막 받은 _id 넘겨주면서 /postlist로 연결되게 -> 그 다음 20개 보내줌
 
+from re import I
 from flask import request, jsonify
 from flask_restx import Resource, Namespace
 from bson import json_util
@@ -60,30 +61,33 @@ class PostListControl(Resource):
         
         if last_id is None:
             #처음 게시판에 들어간 경우(마지막 다큐멘트의 아이디가 없음)
-            cursor = list(mongodb.find(collection_name="board_type_post").sort([("_id", -1)]).limit(page_size))
+            cursor = mongodb.find(collection_name="board_type_post").sort([("_id", -1)]).limit(page_size)
             #최신순으로 정렬해서(내림차순) 그냥 갯수만큼만 불러오면 됨
         else: 
             #사용자가 스크롤을 끝까지 내려서 새로운 리스트를 받아오는 경우
-            cursor = list(mongodb.find(query={'_id': {'$lt': self.last_id} }, collection_name="board_type_post").sort([("_id", -1)]).limit(page_size))
+            cursor = mongodb.find(query={'_id': {'$lt': self.last_id} }, collection_name="board_type_post").sort([("_id", -1)]).limit(page_size)
             #과거의 데이터를 불러오는 거니까 _id가 더 작음(시간이 더 빠르니까)
             #과거의 데이터들 중에서 최신순으로 정렬해서 갯수만큼 불러옴
             #최신순일 수록 _id가 크고 오래될 수록 _id가 작다
         
         
-        post_list = [x for x in cursor]
-        #불러온 다큐멘트들을 리스트형태로 바꿔줌
-
         if not cursor:
             #남은 다큐멘트가 없는 경우
             return None, None
         
-        last_id = str(cursor[-1]['_id'])
+        # last_id = str(cursor[-1]['_id'])
         #해당 리스트의 제일 마지막 요소의 _id값을 받아와야함
-
+        
+        post_list = []
+        for doc in cursor :
+            doc["_id"]=str(doc["_id"])
+            post_list.append(doc)
+        
+        last_id = post_list[-1]["_id"]
 
         return {
-            "post_list" : json.dumps(cursor, default=json_util.default), 
-            "last_id" : last_id
+            "last_id" : last_id, 
+            "post_list" : post_list
         }
         
         # up이 아래에서 위로 -> 새로운 걸 불러오는 것
