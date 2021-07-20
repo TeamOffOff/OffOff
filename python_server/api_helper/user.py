b99ec27613ffbefd56c4ba3c17ddb83cf6c763ca
ssooda <84752537+ssooda@users.noreply.github.com>
2021-07-20 10:52:18 +0900

    사용자 관련 api
    /Auth/register
    아이디 중복 확인 : GET 
     => BODY에 JSON ("id")
    
    회원가입 : POST (/Auth/register) 
    => BODY에 JSON
    
    비밀번호 변경 : PUT (/Auth/register) 
    => HEADER에 Authorization : token / BODY에 JSON("password")
    
    회원탈퇴 : DELETE (/Auth/register)
     =>  HEADER에 Authorization : token
     
    /Auth/login
    로그인 : POST (/Auth/login)  
    => BODY에 JSON ("id", "password")
    
    회원정보 : GET (/Auth/login)  
    =>  HEADER에 Authorization : token
    
    회원정보수정 : PUT (/Auth/login) 
    =>  HEADER에 Authorization : token / BODY에 JSON {"modify" : {"name" : "" , "nickname" : ""}}

--- python_server/api_helper/user.py
+++ python_server/api_helper/user.py
@@ -2,6 +2,7 @@ from flask import request
 from flask_restx import Resource, Api, Namespace, fields
 import jwt
 import bcrypt
+from pymongo import encryption
 
 import mongo
 
@@ -15,7 +16,7 @@ Auth = Namespace(name="Auth", description="유저 관련 API")
 @Auth.route('/register')
 class AuthRegister(Resource):
     """
-    중복확인, 회원가입
+    중복확인, 회원가입, 비밀번호변경, 회원탈퇴
     """
     def get(self):
         """
@@ -30,6 +31,7 @@ class AuthRegister(Resource):
             return{
                 "message": "Possible"
             }, 200
+
 
     def post(self):
         """
@@ -46,16 +48,55 @@ class AuthRegister(Resource):
             'Authorization': token_encoded,
             "message": 'Register Success'
         }, 200
+    
+
+    def put(self):
+        """
+        비밀번호를 변경합니다 
+        비밀번호 변경 클릭 -> 아이디 비밀번호 한 번 더 확인(/Auth/login) -> 새로운 비밀번호 입력 후 변경(/Auth/register)
+        """
+        header = request.headers.get('Authorization')
+        token_decoded = jwt.decode(header, SECRET_KEY, ALGORITHM)
+
+        new_password = request.get_json()
+        new_encrypted_password = bcrypt.hashpw(str(new_password["password"]).encode("utf-8"), bcrypt.gensalt())
+        new_password["password"] = new_encrypted_password.decode('utf-8')  # db저장시에는 디코드
+
+        result = mongodb.update_one(query={"id": token_decoded["id"]}, collection_name="user", modify={"$set": new_password})
+
+        if result.raw_result["n"] == 1:
+            return {"query_status" : "비밀번호 변경을 완료했습니다"}
+        else:
+            return {"query_status": "해당 id의 사용자를 찾을 수 없습니다."}, 500
+
+        
+    def delete(self):
+        """
+        회원정보를 삭제합니다(회원탈퇴)
+        회원탈퇴 클릭 -> 아이디 비밀번호 한 번 더 확인(/Auth/login) -> 회원탈퇴(/Auth/register)
+        """
+        header = request.headers.get('Authorization')
+        # header 에 token_encoded 를 넣어서 request
+        if header is None: 
+            return {"message": "Please Login"}, 404
+        token_decoded = jwt.decode(header, SECRET_KEY, ALGORITHM)  # {'id':실제 id} 딕셔너리형태로 돌려줌
+        result = mongodb.delete_one(query={"id": token_decoded["id"]}, collection_name="user")
+
+        if result.raw_result["n"] == 1:
+            return{"query _status": "success"}
+        else :
+            return {"query_status": "해당 id의 사용자를 찾을 수 없습니다."}, 500
+    
 
 
 @Auth.route('/login')
 class AuthLogin(Resource):
     """
-    로그인, 회원정보
+    로그인, 회원정보, 회원정보수정
     """
-    def post(self):
+    def post(self):  # 로그인
         """
-        입력한 id, password 일치여부를 검사합니다
+        입력한 id, password 일치여부를 검사합니다(로그인 시, 회원탈퇴 위한 아이디, 비밀번호 확인 시)
         """
         sign_info = request.get_json()  # 사용자가 로그인 시 입력한 아이디, 비밀번호
         input_password = sign_info["password"]
@@ -80,7 +121,7 @@ class AuthLogin(Resource):
             }, 200
 
 
-    def get(self):
+    def get(self):  # 회원정보조회
         """
         회원정보를 조회합니다.
         """
@@ -90,5 +131,27 @@ class AuthLogin(Resource):
             return {"message": "Please Login"}, 404
         token_decoded = jwt.decode(header, SECRET_KEY, ALGORITHM)  # {'id':실제 id} 딕셔너리형태로 돌려줌
         user_info = mongodb.find_one(query={"id": token_decoded["id"]}, collection_name="user", projection_key={'_id': 0, "password": 0})
+        
         return user_info,200
+    
+
+    def put(self):  # 회원정보수정
+        """
+        회원정보를 수정합니다
+        """
+        header = request.headers.get('Authorization')
+        token_decoded = jwt.decode(header, SECRET_KEY, ALGORITHM) #인코드된 토큰 받아서 디코드함
+
+        user_info = request.get_json()
+        result = mongodb.update_one(query={"id": token_decoded["id"]}, collection_name="user", modify={"$set": user_info["modify"]})
+
+        if result.raw_result["n"] == 1:
+            modified_user = mongodb.find_one(query={"id": token_decoded["id"]},
+                                             collection_name="user",
+                                             projection_key={"_id": 0, "password":0})
+
+            return modified_user
+        else:
+
+            return {"query_status": "해당 id의 사용자를 찾을 수 없습니다"}, 500
 
