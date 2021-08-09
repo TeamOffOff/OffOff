35ebac9b5d233a672ffbfffad15f46dd102c81fb
Lee Nam Jun <dlskawns96@gmail.com>
2021-08-09 10:56:35 +0900

    로그인 프로세스 RxSswift로 리팩토링

--- OffOff_iOS/OffOff_iOS/View Models/LoginViewModel.swift
+++ OffOff_iOS/OffOff_iOS/View Models/LoginViewModel.swift
@@ -6,26 +6,49 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class LoginViewModel {
-    let loginModel: Box<LoginModel?>
     
-    init() {
-        loginModel = Box(nil)
+    // outputs
+    let isIdConfirmed: Driver<Bool>
+    let isPasswordConfirmed: Driver<Bool>
+    let isSignedIn: Driver<Bool>
+    
+    init(
+        input: (
+            idText: Driver<String>,
+            passwordText: Driver<String>,
+            loginButtonTap: Signal<()>
+        )
+    ) {
+        isIdConfirmed = input.idText
+            .map { $0 != "" }
+            .asDriver(onErrorJustReturn: false)
+        
+        isPasswordConfirmed = input.idText
+            .map { $0 != "" }
+            .asDriver()
+        
+        let idAndPassword = Driver.combineLatest(isIdConfirmed, isPasswordConfirmed)
+        isSignedIn = input.loginButtonTap.withLatestFrom(idAndPassword)
+            .map { $0 && $1 }
+            .asDriver(onErrorJustReturn: false)
     }
     
-    func login(loginStatus: Box<LoginStatus>) {
-        if let loginModel = loginModel.value {
-            print(#fileID, #function, #line, loginModel)
-            let encoder = JSONEncoder()
-            encoder.outputFormatting = .prettyPrinted
-            
-            if let encodedData = try? encoder.encode(loginModel) {
-                // 서버로 아이디 / 비밀번호 보내기
-            }
-            
-            // if loginSuccess
-            loginStatus.value = [LoginStatus.failed, LoginStatus.successed].randomElement()!
-        }
-    }
+//    func login(loginStatus: Box<LoginStatus>) {
+//        if let loginModel = loginModel.value {
+//            print(#fileID, #function, #line, loginModel)
+//            let encoder = JSONEncoder()
+//            encoder.outputFormatting = .prettyPrinted
+//
+//            if let encodedData = try? encoder.encode(loginModel) {
+//                // 서버로 아이디 / 비밀번호 보내기
+//            }
+//
+//            // if loginSuccess
+//            loginStatus.value = [LoginStatus.failed, LoginStatus.successed].randomElement()!
+//        }
+//    }
 }
