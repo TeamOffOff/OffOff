ab1b804dbb503db1fba6320ef50579d8e39a66ef
김소연 <84752537+ssooda@users.noreply.github.com>
2021-08-09 17:37:00 +0900

    게시판 리스트, 게시글 리스트, 게시글, 댓글, 대댓글
    기능 정리

--- python_server/api_helper/board.py
+++ python_server/api_helper/board.py
@@ -3,6 +3,9 @@ from flask_restx import Resource, Namespace
 from bson.objectid import ObjectId
 from datetime import datetime, timedelta
 
+from pymongo import collection
+
+
 import mongo
 
 mongodb = mongo.MongoHelper()
@@ -22,33 +25,34 @@ PostList = Namespace(
 # 사용자가 커뮤니티 탭을 클릭하는 경우
 class BoardListControl(Resource):
 
-    def get(self):
-        """
-        DB > board_list 컬랙션에서 게시판을 조회합니다
-        """
-        """
-        {
-            "_id": 
-            "boardType": "free"
-            "name": "자유게시판"
-            "icon": string(url)
-            "newPost": True
-        }
-        """
+    def get(self):  # 게시판 목록 불러오기
+
         cursor = mongodb.find(collection_name="board_list", projection_key={"_id": 0})
         
-        board_list = []
-        for board in cursor:
-            board_list.append(board)
+        access_on = datetime.now()
+        standard = access_on - timedelta(hours=3)
+
         
-        for board in board_list:
+        board_list= []
+        for board in cursor:
+            
             board_type = board["boardType"]+"_board"
-            access_on = datetime.now()
-            standard = access_on - timedelta(hours=3)
-            if mongodb.aggregate(collection_name=board_type, pipeline={"$match": {"date": {"$gte":standard}}}):
+            
+            result = mongodb.find_one(collection_name=board_type, query={"date":{"$gte": standard}})
+            
+            if result:
                 board["newPost"] = True
-            else:
-                board["newPost"] = False
+
+            # result = mongodb.aggregate(collection_name=board_type, pipeline=[{"$match": {"date": {"$gte":standard}}}])
+            # temp_board_list = []
+            # for new_post in result:
+            #     temp_board_list.append(new_post)
+            
+            # if temp_board_list:
+            #     board["newPost"] = True
+            
+            board_list.append(board)
+                
  
         return {
             "boardList": board_list
@@ -63,9 +67,9 @@ class BoardListControl(Resource):
         result = mongodb.delete_one(query={"boardType": board_type}, collection_name="board_list")
 
         if result.raw_result["n"] == 1:
-            return {"query_status": "해당 게시판을 삭제했습니다."}
+            return {"queryStatus": "해당 게시판을 삭제했습니다."}
         else:
-            return {"query_status": "게시판 삭제를 실패했습니다."}, 500
+            return {"queryStatus": "게시판 삭제를 실패했습니다."}, 500
 
 
     def post(self):  # 게시판 목록 등록
@@ -74,14 +78,14 @@ class BoardListControl(Resource):
 
         mongodb.insert_one(data=board_info, collection_name="board_list")
 
-        return {"query_status": "게시판을 등록했습니다"}
+        return {"queryStatus": "게시판을 등록했습니다"}
 
 
 @PostList.route("/<string:board_type>")
 # 사용자가 특정 게시판을 클릭하는 경우
 class PostListControl(Resource):
 
-    def get(self, board_type):
+    def get(self, board_type):  # 게시글 목록 불러오기
         """
         DB > 해당 게시판의 컬랙션(free_board)에서 게시글을 조회합니다
         """
@@ -102,19 +106,22 @@ class PostListControl(Resource):
             post_list = []
             for post in cursor:
                 post["_id"] = str(post["_id"])
+
+                if board_type != "hot_board":
+                    post["date"] = str(post["date"])
                 post_list.append(post)
 
             last_post_id = post_list[-1]["_id"]
 
-            
-            if board_type is "hot_board":  # 인기게시판인 경우
+            if board_type == "hot_board":  # 인기게시판인 경우
                 hot_post_list = []
                 for temp_post in post_list:
                     post_id = temp_post["_id"]
-                    board_type = temp_post["boardType"]
+                    board_type = temp_post["boardType"]+"_board"
 
                     post = mongodb.find_one(query={"_id": ObjectId(post_id)}, collection_name=board_type)
                     post["_id"] = str(post["_id"])
+                    post["date"] = str(post["date"])
 
                     hot_post_list.append(post)
                 
@@ -130,9 +137,21 @@ class PostListControl(Resource):
                 }
 
                  
-        except IndexError:  # 더 이상 없는 경우
+        except IndexError:  # 일반 게시판 더 이상 없는 경우
             return {
                 "lastPostId": None,
                 "postList": None
             }
+        except TypeError:  # 인기 게시판 더 이상 없는 경우
+            return {
+                "lastPostId": None,
+                "postList": None
+            }
+    
+    def delete(self, board_type):  # 컬렉션 자체를 삭제
+        
+        result= mongodb.drop(collection_name=board_type)
+        
+        return result
+
 
--- python_server/api_helper/post.py
+++ python_server/api_helper/post.py
@@ -1,6 +1,8 @@
 from flask import request
 from flask_restx import Resource, Namespace, fields
 from bson.objectid import ObjectId
+from bson import json_util
+import json
 
 import mongo
 
@@ -16,40 +18,55 @@ SubReply = Namespace("subreply", description="대댓글 관련 API")
 # 게시글 관련 API
 @Post.route("")
 class PostControl(Resource):
-    def get(self):  # 게시글 조회 / 댓글조회 / viewcount+1
-        content_id = request.args.get("content-id")
-        board_type = request.args.get("board-type") + "_board"
-        reply_board_type = request.args.get("board-type")+"_board_reply"
+    def get(self):  # 게시글 조회 / viewcount+1
+        post_id = request.args.get("postId")
+        board_type = request.args.get("boardType") + "_board"
 
-        post = mongodb.find_one(query={"_id": ObjectId(content_id)},
-                                  collection_name=board_type,
-                                  projection_key={"_id": False})
+        # 게시글 조회
+        post = mongodb.find_one(query={"_id": ObjectId(post_id)},
+                                  collection_name=board_type)
+        
 
-        update_status = mongodb.update_one(query={"_id": ObjectId(content_id)},
+        # viewCount +1
+        update_status = mongodb.update_one(query={"_id": ObjectId(post_id)},
                                            collection_name=board_type,
                                            modify={"$inc": {"viewCount": 1}})
 
-        reply = mongodb.find_one(query={"postId": content_id},collection_name=reply_board_type)
-
+        
         if not post:
-            return {"query_status": "해당 id의 게시글을 찾을 수 없습니다."}, 500
+            return {"queryStatus": "not found"}, 500
+
         elif update_status.raw_result["n"] == 0:
-            return {"query_status": "viewCount update fail"}, 500
+            return {"queryStatus": "viewCount update fail"}, 500
+
         else:
-            return {"post": post, "reply": reply}
+            # # 작성자 정보 조회
+            # author = mongodb.find_one(query={"nickname": post["author"]}, collection_name="user", projection_key={"subinfo": True})
+            
+            # if not author:  # 작성자가 탈퇴한 경우
+            #     post["author"] = None
+            # else:
+            #     post["author"] = author
+            post["_id"] = str(post["_id"])
+            post["date"] = str(post["date"])
+
+            return post
+
+
 
     def delete(self):  # 게시글 삭제
         """특정 id의 게시글을 삭제합니다."""
         post_info = request.get_json()
-        content_id = post_info["_id"]
+        post_id = post_info["_id"]
         board_type = post_info["boardType"] + "_board"
 
-        result = mongodb.delete_one(query={"_id": ObjectId(content_id)}, collection_name=board_type)
+        result = mongodb.delete_one(query={"_id": ObjectId(post_id)}, collection_name=board_type)
 
         if result.raw_result["n"] == 1:
-            return {"query_status": "success"}
+            return {"queryStatus": "success"}
         else:
-            return {"query_status": "해당 id의 게시글을 찾을 수 없습니다."}, 500
+            return {"queryStatus": "not found"}, 500
+
 
     def post(self):  # 게시글 생성
         """게시글을 생성합니다."""
@@ -57,21 +74,34 @@ class PostControl(Resource):
         board_type = post_info["boardType"] + "_board"
 
         del post_info["_id"]
-        post_info["date"]=dateutil.parser.parse(post_info["date"])
-
+        post_info["date"] = dateutil.parser.parse(post_info["date"])
+        
         post_id = mongodb.insert_one(data=post_info, collection_name=board_type)
 
-        return {"query_status": str(post_id)}
+        post = mongodb.find_one(query={"_id":post_id}, collection_name=board_type)
+        post["_id"] = str(post["_id"])
+        post["date"] = str(post["date"])
+
+        return post
+
 
     def put(self):  # 게시글 수정
         """특정 id의 게시글을 수정합니다."""
         post_info = request.get_json()
-        content_id = post_info["_id"]
+        post_id = post_info["_id"]
         board_type = post_info["boardType"] + "_board"
+        
+        # 직전 좋아요 수 저장
+        past_likes = mongodb.find_one(query={"_id": ObjectId(post_id)}, collection_name=board_type, projection_key={"_id":False, "likes":True})
+        past_likes = past_likes["likes"]
 
+        # 프론트에서 받은 JSON의 _id 삭제
         del post_info["_id"]
 
+        # string 관련 정보
         article_key = ["title", "content", "image"]
+
+        # integer 관련 정보
         activity_key = ["likes", "viewCount", "reportCount", "replyCount"]
 
         modified_article = {}
@@ -83,36 +113,43 @@ class PostControl(Resource):
             elif key in activity_key:
                 modified_activity[key] = post_info[key]
 
+        # string 수정
         if None in list(modified_activity.values()):
-            result = mongodb.update_one(query={"_id": ObjectId(content_id)},
+            result = mongodb.update_one(query={"_id": ObjectId(post_id)},
                                         collection_name=board_type,
                                         modify={"$set": modified_article})
+
+        # integer 수정
         else:
-            result = mongodb.update_one(query={"_id": ObjectId(content_id)},
+            result = mongodb.update_one(query={"_id": ObjectId(post_id)},
                                         collection_name=board_type,
                                         modify={"$inc": modified_activity})
         
 
 
         if result.raw_result["n"] == 1:
-            modified_post = mongodb.find_one(query={"_id": ObjectId(content_id)},
+            modified_post = mongodb.find_one(query={"_id": ObjectId(post_id)},
                                              collection_name=board_type)
-            modified_post["_id"] = str(modified_post["_id"])
 
-            if modified_post["likes"] >= 10 :
+            # 인기게시판 관련
+            if (past_likes < 10) and (modified_post["likes"] == 10) :
                 hot_post_info={}
                 hot_post_info["_id"] = modified_post["_id"]
                 hot_post_info["boardType"] = modified_post["boardType"]
+                hot_post_info["date"] = modified_post["date"]
 
                 mongodb.insert_one(data=hot_post_info, collection_name="hot_board")
             
-            # 10 -> 9 가 된 경우 삭제하기
-
-
-                
+            elif (past_likes >= 10) and (modified_post["likes"] < 10):
+                mongodb.delete_one(query={"_id": ObjectId(post_id)}, collection_name="hot_board")
+            
+            modified_post["_id"] = str(modified_post["_id"])
+            modified_post["date"] = str(modified_post["date"])
+            
             return modified_post
+
         else:
-            return {"query_status": "해당 id의 게시글을 찾을 수 없습니다."}, 500
+            return {"queryStatus": "not modified"}, 500
 
 
 # 댓글 관련 API
@@ -122,58 +159,17 @@ class CommentControl(Resource):
 
     def post(self):  # 댓글 작성
         """댓글을 생성합니다."""
-        """
-        {
-            "_id":
-            "boardType":
-            "postId":
-            "reply": {
-                "author":
-                "content":
-                "date":
-                "likes":
-            },
-            "subReply":[]
-        }
-        """
         reply_info = request.get_json()
         del reply_info["_id"]
 
         board_type = reply_info["boardType"] + "_board_reply"
 
-        reply_id = mongodb.insert_one(data=reply_info, collection_name=board_type)
+        post_id = reply_info["postId"]
 
-        return {"query_status": str(reply_id)}
+        result = mongodb.insert_one(data=reply_info, collection_name=board_type)
 
-    def get(self):  # 댓글 조회
-        """댓글을 조회합니다."""
-        """
-        {
-            "_id":
-            "boardType":
-            "postId":
-            "reply": {
-                "author":
-                "content":
-                "date":
-                "likes":
-            },
-            "subReply":[
-                {
-                    "author":
-                    "content":
-                    "date":
-                    "likes":
-                }
-            ]
-        }
-        """
-        post_id = request.args.get("postId")
-        board_type = request.args.get("boardType") + "_board_reply"
-
-        cursor = mongodb.find(query={"_id": post_id},
-                              collection_name=board_type,
-                              projection_key={"reply": True, "subReply": True})  # 댓글은 오름차순
+        cursor = mongodb.find(query={"postId": post_id},
+                              collection_name=board_type)  # 댓글은 오름차순
 
         reply_list = []
         for reply in cursor:
@@ -184,9 +180,30 @@ class CommentControl(Resource):
             "reply_list": reply_list
         }
 
+
+    def get(self):  # 댓글 조회
+        """댓글을 조회합니다."""
+
+        post_id = request.args.get("postId")
+        board_type = request.args.get("boardType") + "_board_reply"
+
+        cursor = mongodb.find(query={"postId": post_id},
+                              collection_name=board_type)  # 댓글은 오름차순
+
+        reply_list = []
+        for reply in cursor:
+            reply["_id"] = str(reply["_id"])
+            reply_list.append(reply)
+
+        return {
+            "replyList": reply_list
+        }
+
+
     def delete(self):  # 댓글 삭제
         """댓글을 삭제합니다."""
         reply_info = request.get_json()
+        post_id = reply_info["postId"]
         board_type = reply_info["boardType"] + "_board_reply"
         reply_id = reply_info["_id"]  # 댓글 조회시 해당 댓글 고유의 _id를 포함해서 리턴함
         whether_subreply = reply_info["subReply"]
@@ -206,11 +223,21 @@ class CommentControl(Resource):
             result = mongodb.update_one(query={"_id": ObjectId(reply_id)},
                                         collection_name=board_type,
                                         modify={"$set": alert_delete})
+        
+        cursor = mongodb.find(query={"postId": post_id},
+                              collection_name=board_type)  # 댓글은 오름차순
+
+        reply_list = []
+        for reply in cursor:
+            reply["_id"] = str(reply["_id"])
+            reply_list.append(reply)
 
         if result.raw_result["n"] == 1:
-            return {"query_status": "success"}
+            return {
+            "replyList": reply_list
+        }
         else:
-            return {"query_status": "해당 댓글을 찾을 수 없습니다."}, 500
+            return {"queryStatus": "댓글 삭제에 실패하였습니다."}, 500
 
 
 # 대댓글 관련 API
@@ -221,19 +248,9 @@ class SubcommentControl(Resource):
     def post(self):  # 대댓글 작성
         """
         대댓글 추가
-        {
-                "boardType": free,
-                "replyId": string,
-                "subReply":
-                    {
-                        "author": string,
-                        "content": string,
-                        "date": string?,
-                        "likes": integer
-                    }
-        }
         """
         subreply_info = request.get_json()
+        post_id = subreply_info["postId"]
         reply_id = subreply_info["replyId"]
         board_type = subreply_info["boardType"] + "_board_reply"
         subreply = subreply_info["subReply"]
@@ -242,16 +259,28 @@ class SubcommentControl(Resource):
                                     collection_name=board_type,
                                     modify={"$push": {"subReply": subreply}})
 
+        cursor = mongodb.find(query={"postId": post_id},
+                              collection_name=board_type)  # 댓글은 오름차순
+
+        reply_list = []
+        for reply in cursor:
+            reply["_id"] = str(reply["_id"])
+            reply_list.append(reply)
+
         if result.raw_result["n"] == 1:
-            return {"query_status": "대댓글을 등록했습니다."}
+            return {
+            "replyList": reply_list
+        }
         else:
-            return {"query_status": "대댓글 등록에 실패하였습니다."}, 500
+            return {"queryStatus": "대댓글 등록에 실패하였습니다."}, 500
+
 
     def delete(self):  # 대댓글 삭제
         """
         대댓글 삭제
         """
         subreply_info = request.get_json()
+        post_id = subreply_info["postId"]
         reply_id = subreply_info["replyId"]
         board_type = subreply_info["boardType"] + "_board_reply"
         subreply = subreply_info["subReply"]
@@ -260,7 +289,17 @@ class SubcommentControl(Resource):
                                     collection_name=board_type,
                                     modify={"$pull": {"subReply": subreply}})
 
+        cursor = mongodb.find(query={"postId": post_id},
+                              collection_name=board_type)  # 댓글은 오름차순
+
+        reply_list = []
+        for reply in cursor:
+            reply["_id"] = str(reply["_id"])
+            reply_list.append(reply)
+
         if result.raw_result["n"] == 1:
-            return {"query_status": "대댓글을 삭제하였습니다."}
+            return {
+            "replyList": reply_list
+        }
         else:
-            return {"query_status": "대댓글 삭제에 실패하였습니다."}, 500
+            return {"queryStatus": "대댓글 삭제에 실패하였습니다."}, 500
