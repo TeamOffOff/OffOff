506af52edaee3a00f10dbfff932c91e1e7479f77
Lee Nam Jun <dlskawns96@gmail.com>
2021-09-05 22:00:17 +0900

    AddShift View 기본 구성

--- OffOff_iOS/OffOff_iOS/Controllers/AddShiftViewController.swift
+++ OffOff_iOS/OffOff_iOS/Controllers/AddShiftViewController.swift
@@ -9,17 +9,52 @@ import UIKit
 
 class AddShiftViewController: UIViewController {
 
+    let customView = AddShiftView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let testView = UIView().then { $0.backgroundColor = .white }
-//        self.view.backgroundColor = .lightGray.withAlphaComponent(0.5)
-        self.view.addSubview(testView)
+        self.view.addSubview(customView)
         
-        testView.snp.makeConstraints {
+        customView.snp.makeConstraints {
             $0.centerX.centerY.equalToSuperview()
-            $0.width.height.equalTo(self.view.snp.width).dividedBy(2.0)
+            $0.width.equalTo(self.view.snp.width).dividedBy(1.5)
+            $0.height.equalTo(self.view.snp.width)
         }
     }
+}
+
+class AddShiftView: UIView {
+    var badgeButton = UIButton().then {
+        $0.backgroundColor = .blue
+        $0.setTitle("주", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+    }
     
+    var titleTextField = UITextField().then {
+        $0.text = "타이틀"
+        $0.textColor = .black
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        self.addSubview(badgeButton)
+        self.addSubview(titleTextField)
+        makeView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func makeView() {
+        badgeButton.snp.makeConstraints {
+            $0.left.top.equalToSuperview().inset(20)
+            $0.width.height.equalTo(40)
+        }
+        titleTextField.snp.makeConstraints {
+            $0.centerY.equalTo(badgeButton)
+            $0.right.equalToSuperview().inset(20)
+        }
+    }
 }
