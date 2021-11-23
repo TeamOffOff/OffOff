b56a831796558cf9d989b0795e68666e976a6003
yuuuzzzin <uiuiui118437@gmail.com>
2021-11-23 20:22:51 +0900

    내 정보 / 환경설정 화면 구성

---
+++ OffOff_Android/app/src/main/java/com/yuuuzzzin/offoff_android/views/ui/UserFragment.kt
@@ -0,0 +1,18 @@
+package com.yuuuzzzin.offoff_android.views.ui
+
+import com.yuuuzzzin.offoff_android.R
+import com.yuuuzzzin.offoff_android.databinding.FragmentUserBinding
+import com.yuuuzzzin.offoff_android.utils.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class UserFragment : BaseFragment<FragmentUserBinding>(R.layout.fragment_user) {
+
+    override fun initView() {
+        super.initView()
+    }
+
+    override fun initViewModel() {
+        super.initViewModel()
+    }
+}
