22122e98feb2ed6a5dcbaf90b94c374b42ebf13b
yuuuzzzin <uiuiui118437@gmail.com>
2021-11-26 04:49:55 +0900

    로딩 애니메이션 rotate 방식 수정

--- OffOff_Android/app/src/main/java/com/yuuuzzzin/offoff_android/views/ui/LoadingDialog.kt
+++ OffOff_Android/app/src/main/java/com/yuuuzzzin/offoff_android/views/ui/LoadingDialog.kt
@@ -5,14 +5,24 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import com.yuuuzzzin.offoff_android.R
 
-class LoadingDialog(context: Context) : Dialog(context){
+class LoadingDialog(context: Context) : Dialog(context) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_loading)
         setCancelable(false)
         window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val iv: ImageView = findViewById(R.id.iv_loading)
+        val rotateAnimation = AnimationUtils.loadAnimation(
+            context,
+            R.anim.animation_loading
+        )
+
+        iv.startAnimation(rotateAnimation)
     }
 }
