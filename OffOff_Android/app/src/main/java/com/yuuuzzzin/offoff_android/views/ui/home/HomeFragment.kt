169bdf62e2a6d9f3ecc7651866ba9565205220ba
yuuuzzzin <uiuiui118437@gmail.com>
2021-12-14 20:30:20 +0900

    백 버튼 2번 연속 클릭 시 앱 종료

--- OffOff_Android/app/src/main/java/com/yuuuzzzin/offoff_android/views/ui/home/HomeFragment.kt
+++ OffOff_Android/app/src/main/java/com/yuuuzzzin/offoff_android/views/ui/home/HomeFragment.kt
@@ -1,13 +1,11 @@
 package com.yuuuzzzin.offoff_android.views.ui.home
 
-import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import com.yuuuzzzin.offoff_android.OffoffApplication
 import com.yuuuzzzin.offoff_android.R
 import com.yuuuzzzin.offoff_android.databinding.FragmentHomeBinding
-import com.yuuuzzzin.offoff_android.utils.Constants.toast
 import com.yuuuzzzin.offoff_android.utils.ImageUtils
 import com.yuuuzzzin.offoff_android.utils.base.BaseFragment
 
@@ -16,26 +14,26 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private lateinit var callback: OnBackPressedCallback
     private var backPressedTime: Long = 0
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        callback = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-
-                // 백 버튼 2초 내 다시 클릭 시 앱 종료
-                if (System.currentTimeMillis() - backPressedTime < 2000) {
-                    activity!!.finish()
-                    return
-                }
-
-                // 백 버튼 최초 클릭 시
-                requireContext().toast("뒤로가기 버튼을 한 번 더 누르면 앱이 종료됩니다.")
-                backPressedTime = System.currentTimeMillis()
-
-            }
-        }
-
-        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
-    }
+//    override fun onAttach(context: Context) {
+//        super.onAttach(context)
+//        callback = object : OnBackPressedCallback(true) {
+//            override fun handleOnBackPressed() {
+//
+//                // 백 버튼 2초 내 다시 클릭 시 앱 종료
+//                if (System.currentTimeMillis() - backPressedTime < 2000) {
+//                    activity!!.finish()
+//                    return
+//                }
+//
+//                // 백 버튼 최초 클릭 시
+//                requireContext().toast("뒤로가기 버튼을 한 번 더 누르면 앱이 종료됩니다.")
+//                backPressedTime = System.currentTimeMillis()
+//
+//            }
+//        }
+//
+//        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+//    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
